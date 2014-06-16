cd3df627569965838154e4150cba5b7825f15a97
Aptima <pattern_matching@aptima.com>
2014-06-16 19:12:39 -0400

    Update insertTestData.sh

--- meteorServer/insertTestData.sh
+++ meteorServer/insertTestData.sh
@@ -1,2 +1,1 @@
-curl -H "Content-Type: application/json" -d '{"test": "this"}' codexcom01.cloudapp.net:3000/ingest
-
+curl -H "Content-Type: application/json" -d '{"test": "this"}' codexcom01.cloudapp.net:3000/insert
